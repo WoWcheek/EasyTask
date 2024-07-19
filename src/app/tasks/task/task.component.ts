6ce9866e94b152fa71b7143bbe10b42da40a56a6
Savchuk Volodymyr <savchuk.v03@gmail.com>
2024-07-19 14:50:17 +0300

    add reusable card component

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,11 +1,13 @@
 import { Component, input, output } from '@angular/core';
 import { type Task } from './task.model';
+import { CardComponent } from '../../shared/card/card.component';
 
 @Component({
   selector: 'app-task',
   standalone: true,
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
+  imports: [CardComponent],
 })
 export class TaskComponent {
   task = input.required<Task>();
