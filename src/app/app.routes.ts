5f345610bab0dca4a2938dcf989ee6f1fa087862
Savchuk Volodymyr <savchuk.v03@gmail.com>
2024-07-27 13:19:03 +0300

    implement lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 
-import { routes as userRoutes } from '../app/users/users.routes';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import {
@@ -16,7 +15,8 @@ export const routes: Routes = [
   {
     path: 'users/:userId',
     component: UserTasksComponent,
-    children: userRoutes,
+    loadChildren: () =>
+      import('../app/users/users.routes').then((mod) => mod.routes),
     resolve: {
       userName: resolveUserName,
     },
--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,24 +1,31 @@
 import { type Routes } from '@angular/router';
 
-import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
+import { TasksService } from '../tasks/tasks.service';
 import { NewTaskComponent } from '../tasks/new-task/new-task.component';
+import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'tasks',
-    pathMatch: 'full',
-  },
-  {
-    path: 'tasks',
-    component: TasksComponent,
-    runGuardsAndResolvers: 'always',
-    resolve: {
-      userTasks: resolveUserTasks,
-    },
-  },
-  {
-    path: 'tasks/new',
-    component: NewTaskComponent,
+    providers: [TasksService],
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks',
+        component: TasksComponent,
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+      },
+    ],
   },
 ];
